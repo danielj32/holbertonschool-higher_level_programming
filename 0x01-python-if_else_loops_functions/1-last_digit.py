dbfda908f29bab80621c3976ad23f0070470cc0a
danielj32 <danieljuanito21@hotmail.com>
2020-04-28 23:05:02 +0000

    added new python script new script greater or less

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-# YOUR CODE HERE
 ldigit = 10
 rest = number % ldigit
-
 if rest > 5:
-        print("Last digit of {:d} is {:d}  and is greater than 5".format(number, rest))
+    print("Last digit of {:d} is {:d}".format(number, rest), end=' ')
+    print(" and is greater than 5")
 if rest == 0:
-        print("Last digit of {:d} is {:d}  and is 0".format(number, rest))
+    print("Last digit of {:d} is {:d}  and is 0".format(number, rest))
 elif rest < 6:
-        print("Last digit of {:d} is {:d}  and is less than 6 and not 0".format(number, rest))
+    print("Last digit of {:d} is {:d} ".format(number, rest), end=' ')
+    print("and is less than 6 and not 0")
