82fe7521fcc898ee7a046ceb7d5a977f9e07ae6f
danielj32 <danieljuanito21@hotmail.com>
2020-04-28 23:32:27 +0000

    fixed 2 deleted word

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,9 +3,10 @@ number = random.randint(-10000, 10000)
 # YOUR CODE HERE
 ldigit = 10
 rest = number % ldigit
+
 if number < 0:
     ldigit -= 10
-print("Last digit of {:d} is {:d} ".format(number, ldigit), end='')
+print("Last digit of {:d} is {:d} ".format(number, rest), end='')
 
 if rest > 5:
     print("Last digit of {:d} is {:d}".format(number, rest), end=' ')
