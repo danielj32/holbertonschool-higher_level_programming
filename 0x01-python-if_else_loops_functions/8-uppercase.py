14737317d4e5d2c4b15c9ab5a7f29c09f6691915
danielj32 <danieljuanito21@hotmail.com>
2020-04-29 03:21:42 +0000

    fixed logic added new logic in the code

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def uppercase(str):
-        for k in range(len(str)):
-                upper = str[k]
-        if ord(str[k]) > 96 and ord(str[k]) < 123:
-                upper = chr(ord(str[k]) - 32)
-                print("{}".format(upper), end="")
-print()
+    for i in str:
+        setoffs = 0
+        if ord(i) > 96 and ord(i) < 123:
+            setoffs = -32
+        print("{}".format(chr(ord(i) + setoffs)), end="")
+    else:
+        print()
