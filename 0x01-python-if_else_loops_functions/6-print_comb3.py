14ee60a3fef218b603cd74381786e51e80bc96eb
danielj32 <danieljuanito21@hotmail.com>
2020-04-29 02:20:57 +0000

    added new python script combination of two numbers

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+for n in range(0, 100):
+        i = n % 10
+        j = n / 10
+
+        if j < i and j != i and n != 89:
+                print("{:02d}, ".format(n), end="")
+
+        if n == 89:
+                print("{:d}".format(n))                
+
