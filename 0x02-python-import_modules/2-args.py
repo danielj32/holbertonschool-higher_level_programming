90beb72098b81399ff6956db462c618d768f5068
danielj32 <danieljuanito21@hotmail.com>
2020-04-30 17:56:26 +0000

    fixed in elif added 1 in print

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
                 print("0 arguments.")
         elif len(sys.argv) == 2:
                 print("{} argument:".format(1))
-                print("{}: {}".format(sys.argv[1]))
+                print("{}: {}".format(1, sys.argv[1]))
         else:
                 print("{} arguments:".format(len(sys.argv) - 1))
         for count in range(1, len(sys.argv)):
