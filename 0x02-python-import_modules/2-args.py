34039bfe0a44253fa8279bc036d0665c44d685ca
danielj32 <danieljuanito21@hotmail.com>
2020-04-30 18:08:42 +0000

    fixed logic new logic addapted in conditional

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
+import sys
 if __name__ == "__main__":
-        import sys
-        if len(sys.argv) == 1:
+        from sys import argv
+        count = len(argv)
+        if count == 1:
                 print("0 arguments.")
-        elif len(sys.argv) == 2:
-                print("{} argument:".format(1))
-                print("{}: {}".format(1, sys.argv[1]))
+        elif count == 2:
+                print("1 argument:")
+                print("1: {:s}".format(argv[1]))
         else:
-                print("{} arguments:".format(len(sys.argv) - 1))
-        for count in range(1, len(sys.argv)):
-                        print("{}: {}".format(count,  sys.argv[count]))
+                print("{:d} arguments:".format(count - 1))
+                num = 1
+                for i in range(1, count):
+                        print("{:d}: {:s}".format(num, argv[i]))
+                        num += 1
