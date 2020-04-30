7ca5825e14e81c80452a5a68b8e9493e749ee293
danielj32 <danieljuanito21@hotmail.com>
2020-04-30 17:52:35 +0000

    add new macro add name and main macros

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 import sys
-if len(sys.argv) == 1:
-        print("0 arguments.")
-if len(sys.argv) == 2:
-        print("{} argument:".format(1))
-        print("{}: {}".format(sys.argv[1]))
-else:
-        print("{} arguments:".format(len(sys.argv) - 1))
+if __name__ == "__main__":
+        if len(sys.argv) == 1:
+                print("0 arguments.")
+        if len(sys.argv) == 2:
+                print("{} argument:".format(1))
+                print("{}: {}".format(sys.argv[1]))
+        else:
+                print("{} arguments:".format(len(sys.argv) - 1))
         for count in range(1, len(sys.argv)):
-                print("{}: {}".format(count,  sys.argv[count]))
+                        print("{}: {}".format(count,  sys.argv[count]))
