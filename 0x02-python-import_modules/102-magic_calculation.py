d72439314424a7480ab83b1c5b9178777917dc4c
danielj32 <danieljuanito21@hotmail.com>
2020-05-01 00:51:31 +0000

    added new function magic calculation

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+from magic_calculation_102 import add, sub
+def magic_calculation(a, b):
+        if a < b:
+                c = add(a, b)
+                for j in range(4, 6):
+                        c = add(c, j)
+                return (0)
+        else:
+                return sub(a, b)
