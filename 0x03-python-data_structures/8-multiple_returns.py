6ba6bbe90773899e1d5d081e146a16ef09d54b8d
danielj32 <danieljuanito21@hotmail.com>
2020-05-06 03:17:36 +0000

    fix add add 0 at return

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
         if sentence == '':
-                return (None)
+                return (0, None)
         if sentence:
                 return (len(sentence), sentence[0])
