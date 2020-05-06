ce7fb34e33c761a5028701e43c148a20ee6134e6
danielj32 <danieljuanito21@hotmail.com>
2020-05-06 03:02:57 +0000

    fix add adde new logic and new var

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-        if len(tuple_a) is 0 or len(tuple_a) is 1:
-                tuple_a = (0, 0)
-
-        if len(tuple_b) is 0:
-                tuple_b = (0, 0)
-        elif len(tuple_b) is 1:
-                tuple_b = (tuple_b[0], 0)
-        return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+        tuple_a += (0, 0)
+        tuple_b += (0, 0)
+        tup_a = tuple_a[0] + tuple_b[0]
+        tup_b = tuple_b[1] + tuple_b[1]
+        return (tup_a, tup_b)
