7b9a5e01ec9f52e5d994316139734379827c73d1
danielj32 <danieljuanito21@hotmail.com>
2020-05-06 03:05:25 +0000

    fix add fix name var

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -3,5 +3,5 @@ def add_tuple(tuple_a=(), tuple_b=()):
         tuple_a += (0, 0)
         tuple_b += (0, 0)
         tup_a = tuple_a[0] + tuple_b[0]
-        tup_b = tuple_b[1] + tuple_b[1]
+        tup_b = tuple_a[1] + tuple_b[1]
         return (tup_a, tup_b)
