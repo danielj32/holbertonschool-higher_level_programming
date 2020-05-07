5051f68f587f17cee7f6c136c64d6fd9fc2a47ff
danielj32 <danieljuanito21@hotmail.com>
2020-05-07 23:46:07 +0000

    fix add 2 add new print format

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,4 +2,4 @@
 def print_sorted_dictionary(a_dictionary):
         sorted(a_dictionary.keys())
         for k in sorted(a_dictionary.keys()):
-                print(k, ":", a_dictionary[k])
+                print("{:s}: {}" .format(k, a_dictionary[k]))
