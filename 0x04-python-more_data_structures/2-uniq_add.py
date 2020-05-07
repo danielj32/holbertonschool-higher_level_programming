045619cebe0d0a0667c8417115dbb057e538b2a9
danielj32 <danieljuanito21@hotmail.com>
2020-05-07 22:08:14 +0000

    fix add added set

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
+        cpy_list = set(my_list)
         result = 0
-        for i in my_list:
+        for i in cpy_list:
                 result = result + i
         return result
