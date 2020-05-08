eb43cc5eb13c85dc59580e12145bee666890e0f0
danielj32 <danieljuanito21@hotmail.com>
2020-05-08 02:37:50 +0000

    added new function average

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+        tot = 0
+        d = 0
+        if my_list == "":
+                return 0
+        for y, z in my_list:
+                tot += z
+                d += y * z
+        return d / tot
