7802604a41321a0b849a02120ea13dadfbcedf15
danielj32 <danieljuanito21@hotmail.com>
2020-05-18 18:44:23 +0000

    fix add 2 add if  n < x

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,8 +3,9 @@ def safe_print_list(my_list=[], x=0):
     try:
         n = 0
         for ct in my_list:
-            print("{}".format(ct), end="")
-            n += 1
+            if n < x:
+                print("{}".format(ct), end="")
+                n += 1
         print('')
         return n
     except SyntaxError:
