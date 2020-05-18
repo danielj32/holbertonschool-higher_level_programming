59067518b315b69d31b7a226471a171ed9d34731
danielj32 <danieljuanito21@hotmail.com>
2020-05-18 16:54:39 +0000

    added new directory and new function print the list with exception SintaxError

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    try:
+        n = 0
+        for ct in my_list:
+            if n < x:
+                print("{}".format(ct), end="")
+                n += 1
+                print('')
+                return n
+    except SyntaxError:
+        print("error solution")
---
+++ 0x05-python-exceptions/README.md
@@ -0,0 +1,1 @@
+This directory contents information about Exceptions in Python.
