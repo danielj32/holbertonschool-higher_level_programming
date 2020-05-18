11694fb50130eee45c2a2e7d9e6823ef29d78fee
danielj32 <danieljuanito21@hotmail.com>
2020-05-18 20:52:32 +0000

    added new function print elements of list

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    n = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+        except (TypeError, ValueError):
+            continue
+        else:
+            n += 1
+    print()
+    return n
