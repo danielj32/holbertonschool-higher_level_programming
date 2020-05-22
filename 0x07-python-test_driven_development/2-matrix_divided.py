1ba13a3cd6d4c14e48d2e5c405ca2fd23333cbd5
danielj32 <danieljuanito21@hotmail.com>
2020-05-22 19:22:26 +0000

    add new function divide a matrix

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,10 +1,19 @@
 #!/usr/bin/python3
 def matrix_divided(matrix, div):
-        matrix = [[]]
         if type(div) is not int and type(div) is not float:
                 raise TypeError("div must be a number")
         if div == 0:
                 raise ZeroDivisionError("division by zero")
-        for row in matrix:
-                for fills in row:
-                        return fills
+        if type(matrix) is not list or len(matrix) == 0:
+                raise TypeError("matrix must be a matrix (list of lists) \
+                         of integers/floats")
+        n_matrix = matrix.copy()
+        first = []
+        second = []
+
+        for ct in n_matrix[0]:
+                first.append(round(ct/div, 2))
+        for ct2 in n_matrix[1]:
+                second.append(round(ct2 / div, 2))
+        n_matrix = [first, second]
+        return n_matrix
