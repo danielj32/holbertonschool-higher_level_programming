0d95eda44f9f9f9233819fba3be128a47a48fabb
danielj32 <danieljuanito21@hotmail.com>
2020-05-23 05:37:01 +0000

    fix add change name function test

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -7,41 +7,41 @@ max_integer = __import__('6-max_integer').max_integer
 class TestMaxInteger(unittest.TestCase):
         """ Class of testing """
 
-        def first_test(self):
+        def test_first_test(self):
                 n_list = [100, 200, 400]
                 self.assertEqual(max_integer(n_list), 400)
 
-        def second_test(self):
+        def test_second_test(self):
                 n_list = [-100, 200, 400]
                 self.assertEqual(max_integer(n_list), 400)
 
-        def third_test(self):
+        def test_third_test(self):
                 n_list = [1]
                 self.assertEqual(max_integer(n_list), 1)
 
-        def four_test(self):
+        def test_four_test(self):
                 n_list = []
                 self.assertEqual(max_integer(n_list), None)
 
-        def five_test(self):
+        def test_five_test(self):
                 n_list = [20.4, 34.5, 38.7]
                 self.assertEqual(max_integer(n_list), 38.7)
 
-        def six_test(self):
+        def test_six_test(self):
                 n_list = [20.4, 'm', 38.7]
                 with self.assertRaises(TypeError):
                         max_integer(n_list)
 
-        def seven_test(self):
+        def test_seven_test(self):
                 n_list = ['j', 22, 38.7]
                 with self.assertRaises(TypeError):
                         max_integer(n_list)
 
-        def eight_test(self):
+        def test_eight_test(self):
                 n_list = [-34, -22, 0]
                 self.assertEqual(max_integer(n_list), 0)
 
-        def nine_test(self):
+        def test_nine_test(self):
                 n_list = "oz"
                 self.assertEqual(max_integer(n_list), 'z')
 
