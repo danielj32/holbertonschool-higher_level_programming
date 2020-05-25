40d32380ad151469acc76b454b5c10e77505d32b
danielj32 <danieljuanito21@hotmail.com>
2020-05-25 23:41:14 +0000

    fix 3 change order logic

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -5,29 +5,29 @@
 class Rectangle:
     """show the class Rectangule"""
     def __init__(self, width=0, height=0):
-        self.height = height
         self.width = width
-
-    @property
-    def height(self):
-        return self.__height
-
-    @height.setter
-    def height(self, h):
-        if type(h) is not int:
-            raise TypeError("height must be an integer")
-        if h < 0:
-            raise ValueError("height must be >= 0")
-        self.__height = h
+        self.height = height
 
     @property
     def width(self):
         return self.__width
 
     @width.setter
-    def width(self, w):
-        if type(w) is not int:
+    def width(self, width):
+        if type(width) is not int:
             raise TypeError("width must be an integer")
-        if w < 0:
-            raise TypeError("width must be >= 0")
-        self.__width = w
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, height):
+        if type(height) is not int:
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = height
