5b812d087ef2db335634a319bf9d344a02cb236e
danielj32 <danieljuanito21@hotmail.com>
2020-06-02 23:11:19 +0000

    new fix add commit

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""class MyList """
+
+
 class MyList(list):
+    """show the class Rectangule"""
 
     def print_sorted(self):
         """prints the list, but sorted (ascending sort) """
