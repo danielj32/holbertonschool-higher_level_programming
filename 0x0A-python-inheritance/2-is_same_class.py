192d1eb8b4e6a66563cfe56c607e77491f08048d
danielj32 <danieljuanito21@hotmail.com>
2020-06-02 23:19:26 +0000

    fix update add commit

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+""" function check if object or class"""
+
+
 def is_same_class(obj, a_class):
     """True if the object is exactly an instance of the specified class"""
     return type(obj) == a_class
