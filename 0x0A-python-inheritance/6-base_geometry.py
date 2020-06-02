a1b03abcbbfad165fd0b4886e203f44a2a75d7af
danielj32 <danieljuanito21@hotmail.com>
2020-06-02 23:55:21 +0000

    add new class and function add raise exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+#!/usr/bin/python3
+"""define a class BaseGeometry"""
+
+
+class BaseGeometry:
+    """show the class BaseGeometry"""
+    def area(self):
+        """show the area"""
+        raise Exception(area() is not implemented)
