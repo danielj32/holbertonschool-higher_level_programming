86597ef8550cd00d2a14374c417ee0ac339f6c17
danielj32 <danieljuanito21@hotmail.com>
2020-06-03 23:37:22 +0000

    add json function create a object to a text file

---
+++ 0x0B-python-input_output/8-load_from_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" create an Object to a text file  """
+import json
+
+
+def load_from_json_file(filename):
+    """ creates an
+    Object from a “JSON file” """
+    with open(filename, 'r') as f:
+        return json.load(f)
