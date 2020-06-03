fbed2df227186c75f26dbf7143fc00d14238c257
danielj32 <danieljuanito21@hotmail.com>
2020-06-03 00:16:20 +0000

    add class add if conditionals

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""define a class BaseGeometry"""
+
+
+class BaseGeometry:
+    """show the class BaseGeometry"""
+    def area(self):
+        """show the area"""
+        raise Exception('area() is not implemented')
+
+    def integer_validator(self, name, value):
+        """ area integer validation """
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        elif value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
