14484d21c821189d3830798fd381ba1766e050c8
danielj32 <danieljuanito21@hotmail.com>
2020-06-03 22:36:30 +0000

    add function return a json object

---
+++ 0x0B-python-input_output/6-from_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" return json an object """
+import json
+
+
+def from_json_string(my_str):
+    """ returns an object
+    (Python data structure)
+    represented by a JSON string:
+    """
+    return json.loads(my_str)
