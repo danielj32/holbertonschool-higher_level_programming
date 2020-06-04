985943f4a9514acad0102178be7481018a77b31a
danielj32 <danieljuanito21@hotmail.com>
2020-06-04 00:36:07 +0000

    add json function add all arguments to a Python list

---
+++ 0x0B-python-input_output/9-add_item.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" adds all arguments to a Python list  """
+import json
+import sys
+
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+
+if __name__ == "__main__":
+    try:
+        lista_json = load_from_json_file('add_item.json')
+    except FileNotFoundError:
+        lista_json = []
+
+    for j in range(1, len(sys.argv)):
+        lista_json.append(sys.argv[1])
+    save_to_json_file(lista_json, "add_item.json")
