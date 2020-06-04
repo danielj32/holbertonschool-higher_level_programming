a2f96759df8a5c73f0ae7dddf1d2b889d1fb885a
danielj32 <danieljuanito21@hotmail.com>
2020-06-04 02:38:07 +0000

    add class add Public method

---
+++ 0x0B-python-input_output/13-student.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" Class Student """
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        """ Construct """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """ Class to dictionary """
+        return self.__dict__
+
+    def to_json(self, attrs=None):
+        """ returns directory description with filter """
+
+        if attrs is None or not isinstance(attrs, list):
+            return self.__dict__
+        r = {}
+        for ct in attrs:
+            if not isinstance(ct, str):
+                return self.__dict__
+            if ct in self.__dict__.keys():
+                r[ct] = self.__dict__[ct]
+        return r
+
+    def reload_from_json(self, json):
+        """ Replaces attributes """
+        for ct in json:
+            self.__dict__[ct] = json[ct]
