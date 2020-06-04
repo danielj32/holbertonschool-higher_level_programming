285d58691a2ee46560495533692841f6b1f80714
danielj32 <danieljuanito21@hotmail.com>
2020-06-04 01:56:38 +0000

    add class return dictionary

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Class Student """
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        """ Construct """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """ Class to dictionary """
+        return self.__dict__
