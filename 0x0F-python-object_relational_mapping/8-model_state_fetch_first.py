08fb466557bb1e5130f797b4c18106dbac7dd0f1
danielj32 <danieljuanito21@hotmail.com>
2020-08-10 04:56:53 +0000

    fix add 4

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -3,20 +3,21 @@
 State object from the database hbtn_0e_6_usa
 """
 
-
 if __name__ == "__main__":
-    import sys
-    from model_state import Base, State
-    from sqlalchemy.orm import sessionmaker
-    from sqlalchemy import (create_engine)
-    en = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                       .format(sys.argv[1], sys.argv[2],
-                               sys.argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(en)
-    session = Session(en)
-    state1 = session.query(State).order_by(State.id).first()
-    if state1:
-        print("{}: {}".format(state1.id, state1.name))
-    else:
-        print("Nothing")
-    session.close()
+    try:
+        import sys
+        from model_state import Base, State
+        from sqlalchemy import create_engine
+        from sqlalchemy.orm import Session
+        en = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2],
+                                   sys.argv[3]), pool_pre_ping=True)
+        Base.metadata.create_all(en)
+        session = Session(en)
+        state1 = session.query(State).order_by(State.id).first()
+        if state1:
+            print("{}: {}".format(state1.id, state1.name))
+        else:
+            print("Nothing")
+    except:
+        pass
