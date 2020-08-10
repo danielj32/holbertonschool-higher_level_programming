1c9c182c6421b539bdc00e56e29587d73766c7c5
danielj32 <danieljuanito21@hotmail.com>
2020-08-10 04:54:30 +0000

    fix add 3

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -2,11 +2,13 @@
 """script that prints the first
 State object from the database hbtn_0e_6_usa
 """
-import sys
-from model_state import Base, State
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import (create_engine)
+
+
 if __name__ == "__main__":
+    import sys
+    from model_state import Base, State
+    from sqlalchemy.orm import sessionmaker
+    from sqlalchemy import (create_engine)
     en = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
                        .format(sys.argv[1], sys.argv[2],
                                sys.argv[3]), pool_pre_ping=True)
