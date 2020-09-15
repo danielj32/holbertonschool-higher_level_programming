280d54022a96613baaecfa538efc6353b3f05d0a
danielj32 <danieljuanito21@hotmail.com>
2020-09-15 22:44:52 +0000

    fix add

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-let myVar = 'C is fun';
-let myVar2 = 'Python is cool';
-let myVar3 = 'Javascript is amazing';
+const myVar = 'C is fun';
+const myVar2 = 'Python is cool';
+const myVar3 = 'Javascript is amazing';
 console.log(myVar);
 console.log(myVar2);
 console.log(myVar3);
