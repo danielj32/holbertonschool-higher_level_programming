914ac249c47fd457daf6d801a77cb3f8c9393ff2
danielj32 <danieljuanito21@hotmail.com>
2020-09-22 02:54:04 +0000

    add new task

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+exports.esrever = function (list) {
+  const listReversed = [];
+  for (let i = list.length - 1; i >= 0; i--) {
+    listReversed.push(list[i]);
+  }
+  return listReversed;
+};
